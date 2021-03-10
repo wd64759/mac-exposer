55eeac4a7b766444082acfe6e7828661b0a69b4b
wei123 <dongw1980@hotmail.com>
2021-03-10 20:55:39 +0800

    update gradle script to build fat jar

--- src/main/java/com/cte4/mac/machelper/MACHelperMain.java
+++ src/main/java/com/cte4/mac/machelper/MACHelperMain.java
@@ -1,6 +1,6 @@
 package com.cte4.mac.machelper;
 
-public class AppMain {
+public class MACHelperMain {
     public static void main(String[] args) {
         System.out.println("prompt:TODO");
     }
