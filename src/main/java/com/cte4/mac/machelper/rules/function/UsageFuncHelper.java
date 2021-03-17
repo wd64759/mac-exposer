b0463a5f4b72a44b175201e705ab175df54b9aae
18616682280 <wei.dong120@icloud.com>
2021-03-17 11:21:00 +0800

    update usage case

--- src/main/java/com/cte4/mac/machelper/rules/function/UsageFuncHelper.java
+++ src/main/java/com/cte4/mac/machelper/rules/function/UsageFuncHelper.java
@@ -1,5 +1,37 @@
 package com.cte4.mac.machelper.rules.function;
 
-public class UsageFuncHelper {
+import com.cte4.mac.machelper.model.MetricsEntity;
+import com.cte4.mac.machelper.utils.AgentConnector;
+
+import org.jboss.byteman.rule.Rule;
+import org.jboss.byteman.rule.helper.Helper;
+
+import io.prometheus.jmx.shaded.io.prometheus.client.GaugeMetricFamily;
+
+import static java.util.Arrays.asList;
+
+public class UsageFuncHelper extends Helper {
+
+    static final String RULE_NAME = "USAGE_FNC";
+
+    protected UsageFuncHelper(Rule rule) {
+        super(rule);
+    }
     
+    public void enableMonitor() {
+        System.out.println("::disable rule::" + RULE_NAME);
+    }
+
+    public static void uninstalled(Rule rule) {
+        System.out.println("::disable rule::" + RULE_NAME);
+    }
+
+    public void sendMetrics(String clz, String method, long timecost) {
+        MetricsEntity metrics = MetricsEntity.funcBundle(RULE_NAME);
+        GaugeMetricFamily tsGauge = new GaugeMetricFamily(String.format("mac_perf_latency_milliseconds", method),
+                "Time cost of method process.", asList("class","method","rulename"));
+        tsGauge = tsGauge.addMetric(asList(clz, method, RULE_NAME), timecost);
+        metrics.getMetrics().add(tsGauge);
+        AgentConnector.build().sendMessage(metrics);
+    }
 }
