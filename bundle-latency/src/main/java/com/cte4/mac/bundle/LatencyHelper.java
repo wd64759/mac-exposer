534b67093631a016ce4c3ef34c7c0afbb7200aaa
18616682280 <wei.dong120@icloud.com>
2021-05-11 12:44:39 +0800

    re-structure as multi sub projects

---
+++ bundle-latency/src/main/java/com/cte4/mac/bundle/LatencyHelper.java
@@ -0,0 +1,10 @@
+package com.cte4.mac.bundle;
+
+import com.cte4.mac.exposer.model.CmdEntity;
+
+public class LatencyHelper {
+
+    public static void main(String[] args) {
+        // CmdEntity ce = new CmdEntity();
+    }
+}
